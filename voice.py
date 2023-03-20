8769a3fcc339091d57920b569f6a4104b2c6aedf
jameshasung <117165485+jameshasung@users.noreply.github.com>
2023-03-20 12:07:15 +0900

    feat: GPT openAPI 기능 추가

--- voice.py
+++ voice.py
@@ -1,8 +1,31 @@
 # 실행 전 모듈 먼저 설치
-# (mac)brew install portaudio && pip install pyaudio
+# brew install portaudio && pip install pyaudio
+# pip install openai
 
 import speech_recognition as sr
 import pyaudio
+import openai
+
+openai.api_key = "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"
+
+
+def get_openai_response(prompt, print_output=False):
+
+    completions = openai.Completion.create(
+        engine='text-davinci-003',  # Determines the quality, speed, and cost.
+        temperature=0.5,            # Level of creativity in the response
+        prompt=prompt,           # What the user typed in
+        max_tokens=3072,             # Maximum tokens in the prompt AND response
+        n=1,                        # The number of completions to generate
+        stop=None,                  # An optional setting to control response generation
+    )
+    # Displaying the output can be helpful if things go wrong
+    if print_output:
+        print(completions)
+
+    # Return the first choice's text
+    return completions.choices[0].text
+
 
 # 음성 입력 설정
 r = sr.Recognizer()
@@ -18,3 +41,6 @@ except sr.UnknownValueError:
     print("음성을 인식할 수 없습니다.")
 except sr.RequestError as e:
     print("구글 음성 인식 API에 접근할 수 없습니다: {0}".format(e))
+
+print("OpenAI GPT-3로 음성을 입력합니다. . . . .")
+print("OpenAI GPT-3의 응답: " + get_openai_response(text))
